f2a3649fac12ec91d83e864066ac33dbafd76ea8
Ross Wilson <rzzzwilson@gmail.com>
2016-10-24 12:46:32 +0700

    Fiddling with polygon faces

--- coloured_dodecahedron.py
+++ coloured_dodecahedron.py
@@ -72,16 +72,16 @@ vertices = (
 surfaces = (
             (0, 13, 3, 18, 19),
             (0, 9, 1, 12, 13),
-#            (1, 16, 17, 2, 12),
+            (1, 16, 17, 2, 12),
             (2, 10, 3, 13, 12),
-#            (0, 19, 4, 8, 9),
-#            (1, 9, 8, 5, 16),
-#            (2, 17, 6, 11, 10),
-#            (3, 18, 7, 11, 10),
-#            (7, 14, 4, 19, 18),
-#            (5, 15, 6, 17, 16),
-#            (5, 15, 14, 4, 8),
-#            (7, 14, 15, 6, 11),
+            (0, 19, 4, 8, 9),
+            (1, 9, 8, 5, 16),
+            (2, 17, 6, 11, 10),
+            (3, 18, 7, 11, 10),
+            (7, 14, 4, 19, 18),
+            (5, 15, 6, 17, 16),
+            (5, 15, 14, 4, 8),
+            (7, 14, 15, 6, 11),
            )
 edges = (
          (0, 9),
@@ -116,30 +116,60 @@ edges = (
          (18, 19),
         )
 
-def Dodecahedron():
-    glBegin(GL_TRIANGLE_FAN)
-    for surface in surfaces:
-        x = 0
-        for vertex in surface:
-            x += 1
-            if x >= len(colors):
-                x = 0
-            glColor3fv(colors[x])
-            glVertex3fv(vertices[vertex])
-    glEnd()
+fly = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+       0x03, 0x80, 0x01, 0xC0, 0x06, 0xC0, 0x03, 0x60,
+       0x04, 0x60, 0x06, 0x20, 0x04, 0x30, 0x0C, 0x20,
+       0x04, 0x18, 0x18, 0x20, 0x04, 0x0C, 0x30, 0x20,
+       0x04, 0x06, 0x60, 0x20, 0x44, 0x03, 0xC0, 0x22,
+       0x44, 0x01, 0x80, 0x22, 0x44, 0x01, 0x80, 0x22,
+       0x44, 0x01, 0x80, 0x22, 0x44, 0x01, 0x80, 0x22,
+       0x44, 0x01, 0x80, 0x22, 0x44, 0x01, 0x80, 0x22,
+       0x66, 0x01, 0x80, 0x66, 0x33, 0x01, 0x80, 0xCC,
+       0x19, 0x81, 0x81, 0x98, 0x0C, 0xC1, 0x83, 0x30,
+       0x07, 0xe1, 0x87, 0xe0, 0x03, 0x3f, 0xfc, 0xc0,
+       0x03, 0x31, 0x8c, 0xc0, 0x03, 0x33, 0xcc, 0xc0,
+       0x06, 0x64, 0x26, 0x60, 0x0c, 0xcc, 0x33, 0x30,
+       0x18, 0xcc, 0x33, 0x18, 0x10, 0xc4, 0x23, 0x08,
+       0x10, 0x63, 0xC6, 0x08, 0x10, 0x30, 0x0c, 0x08,
+       0x10, 0x18, 0x18, 0x08, 0x10, 0x00, 0x00, 0x08]
 
-    glBegin(GL_LINES)
+def Dodecahedron():
+#    glCullFace(GL_BACK)
+#    glPolygonMode(GL_FRONT, GL_FILL)
+#    glPolygonMode(GL_BACK, GL_FILL)
+#    glPolygonMode(GL_FRONT, GL_FILL)
+#    glEnable(GL_CULL_FACE)
+
+#    glFrontFace(GL_CW)
+#    glEnable (GL_POLYGON_STIPPLE)
+#    glPolygonStipple(fly)
+    x = 0
+    for surface in surfaces:
+        x += 1
+        if x >= len(colors):
+            x = 0
+        glColor3fv(colors[x])
+        glBegin(GL_POLYGON)
+        for vertex in surface:
+            glVertex3fv(vertices[vertex])
+        glEnd()
+
+    glPushAttrib(GL_ENABLE_BIT)
+    glLineStipple(2, 0xAAAA)
+    glEnable(GL_LINE_STIPPLE)
     glColor3fv((1.0, 1.0, 1.0))
     for edge in edges:
+        glBegin(GL_LINES)
         for vertex in edge:
             glVertex3fv(vertices[vertex])
-    glEnd()
+        glEnd()
+    glPopAttrib()
 
 def main():
     pygame.init()
-    display = (800,600)
+    display = (800, 600)
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
-    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+    gluPerspective(45, (float(display[0])/display[1]), 0.1, 50.0)
     glTranslatef(0.0,0.0, -5)
     while True:
         for event in pygame.event.get():
@@ -150,8 +180,7 @@ def main():
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         Dodecahedron()
         pygame.display.flip()
-        #pygame.time.wait(10)
-        pygame.time.wait(100)
+        pygame.time.wait(10)
+        #pygame.time.wait(100)
 
 main()
-
