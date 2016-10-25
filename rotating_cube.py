5ba1a9caf5d112c4515ffa932b8b7c2e2eec9246
Ross Wilson <rzzzwilson@gmail.com>
2016-10-25 22:44:28 +0700

    Fixed aspect ratio

--- rotating_cube.py
+++ rotating_cube.py
@@ -41,7 +41,7 @@ def main():
     pygame.init()
     display = (800,600)
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
-    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+    gluPerspective(45, (float(display[0])/display[1]), 0.1, 50.0)
     glTranslatef(0.0,0.0, -5)
     while True:
         for event in pygame.event.get():
